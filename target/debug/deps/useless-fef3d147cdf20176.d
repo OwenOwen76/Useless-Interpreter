872f28ad21911154e820e64e972b807397a2fa54
Owen <owenzh12@gmail.com>
2026-07-06 14:49:05 -0700

    Untracked /target

--- target/debug/deps/useless-fef3d147cdf20176.d
+++
@@ -1,8 +0,0 @@
-/home/owen/Coding/useless/target/debug/deps/useless-fef3d147cdf20176.d: src/main.rs src/compiler.rs src/lexer.rs src/parser.rs
-
-/home/owen/Coding/useless/target/debug/deps/libuseless-fef3d147cdf20176.rmeta: src/main.rs src/compiler.rs src/lexer.rs src/parser.rs
-
-src/main.rs:
-src/compiler.rs:
-src/lexer.rs:
-src/parser.rs:
